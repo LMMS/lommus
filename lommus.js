c439a11cf1a8df61c31586aab9a82984a08581e1
Markus Mafz <65797058+headquarter8302@users.noreply.github.com>
2025-10-17 20:50:48 -0400

    Wrap logic into `LoMMuS` class
    This commit wraps the entirety of `lommus.js`'s logic into the `LoMMuS` class. This is the first step to completely modularizing the bot.
    
    Also, enable TS strict null checks

--- lommus.js
+++ lommus.js
@@ -1,141 +1,172 @@
 import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname.concat('/.env'), quiet: true });
+import { ActivityType, Client, Collection, EmbedBuilder, Events, GatewayIntentBits, Partials } from 'discord.js';
 import fs from 'node:fs';
-import { Client, Events, Collection, GatewayIntentBits, EmbedBuilder, Partials, ActivityType } from 'discord.js';
 import config from './config.json' with { type: 'json' };
 
-const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildModeration,
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.DirectMessages,
-	],
-	partials: [
-		Partials.Message,
-		Partials.Channel,
-		Partials.Reaction,
-	],
-});
+console.log("LoMMuS is initializing...");
 
-// Fires when bot successfully authenticates via token
-client.once(Events.ClientReady, async () => {
-	// Get guild from client in order to set initial activity status
-	const guild = client.guilds.cache.get(config.guildId);
+class LoMMuS {
+	/**
+	 * The client class instantiated and cached
+	 * @type {Client}
+	 */
+	client = new Client({
+		intents: [
+			GatewayIntentBits.Guilds,
+			GatewayIntentBits.GuildMembers,
+			GatewayIntentBits.GuildMessages,
+			GatewayIntentBits.GuildModeration,
+			GatewayIntentBits.GuildMessageReactions,
+			GatewayIntentBits.MessageContent,
+			GatewayIntentBits.DirectMessages,
+		],
+		partials: [
+			Partials.Message,
+			Partials.Channel,
+			Partials.Reaction,
+		],
+	});
 
-	if (!client.user) {
-		console.error("client.user not defined! Did the authentication fail?");
-		return;
-	}
-	if (!guild) {
-		console.error("guild is not defined! Is the bot joined to any server?");
-		return;
-	}
+	/**
+	 * Cache color configuration here + TS assertions
+	 * @constant
+	 */
+	colors = {
+		RED: /** @type {`#${string}`} */ (config.red),
+		GREEN: /** @type {`#${string}`} */ (config.green)
+	};
 
-	console.log(`Ready! Logged in as ${client.user.tag}`);
-
-	client.user.setActivity(`${guild.memberCount} LeMMingS`, { type: ActivityType.Watching });
-
-	// Collect module files from directory
-	client.addons = new Collection();
-	const addonFiles = fs.readdirSync('./modules').filter(file => file.endsWith('.mjs'));
-	// Loop Collection of module files
-	for (const file of addonFiles) {
-		// Map
-		const addon = require(`./modules/${file}`);
-		client.addons.set(addon.name, addon);
-
-		// Execute module.export code from module files
-		try {
-			client.addons.get(addon.name).execute(client);
-			console.log(addon.name + ' module loaded.');
-		}
-		catch (error) {
-			console.error(error);
-		}
-	}
-});
-
-// Fires once for each slash command sent by users
-client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction || !interaction.channel || !interaction.guild) {
-		console.error("Interaction is not configured correctly! Has slash commands been registered yet?");
-		return;
+	/**
+	 * Initializes LoMMuS and logs in
+	 * @param {string} token
+	 */
+	constructor (token) {
+		console.log("Instantiating LoMMuS...");
+		this.setupBot();
+		this.setupSlashCommands();
+		this.client.login(token);
 	}
 
-	// Screen bad command interactions
-	if (!interaction.isChatInputCommand()) return;
+	/**
+	 * Loads CommonJS-style modules from the `./modules` directory.
+	 */
+	loadCJSModules() {
+		// Collect module files from directory
+		this.client.addons = new Collection();
+		const addonFiles = fs.readdirSync('./modules').filter(file => file.endsWith('.mjs'));
+		// Loop Collection of module files
+		for (const file of addonFiles) {
+			// Map
+			const addon = require(`./modules/${file}`);
+			this.client.addons.set(addon.name, addon);
 
-	// Restart bot
-	if (interaction.commandName === 'restart') {
-		const embed = new EmbedBuilder()
-			.setAuthor({ name: 'Restarting', iconURL: interaction.guild.iconURL({ size: 64 }) })
-			.setColor(config.red)
-			.setDescription('Was I a Good Bot?');
-
-		await interaction.reply({ embeds: [embed], ephemeral: true })
-			// Exit process, loop.sh container will restart bot automatically
-			.then(async () => {
-				console.log("Exiting...");
-				process.exit(0);
-			})
-			.catch(error => {
-				console.error('Unable to restart!', error);
-			});
-	}
-
-	// Chat as bot
-	if (interaction.commandName === 'say') {
-		const msg = interaction.options.getString('message');
-
-		// await interaction.reply({ content: 'Done. Dismiss this message.', ephemeral: true });
-		interaction.channel.send({ content: msg });
-	}
-
-	// Toggle various global booleans
-	if (interaction.commandName === 'toggle') {
-		const toggleType = interaction.options.getString('function');
-		// Color randomization toggle
-		if (toggleType === 'toggle_color') {
-			// flip
-			global.colorRandom = !global.colorRandom;
-
-			const embed = new EmbedBuilder()
-				.setColor(config.red)
-				.setDescription('Color randomization disabled.');
-			if (global.colorRandom) {
-				embed.setColor(config.green);
-				embed.setDescription('Color randomization enabled.');
+			// Execute module.export code from module files
+			try {
+				this.client.addons.get(addon.name).execute(this.client);
+				console.log(addon.name + ' module loaded.');
 			}
-			await interaction.reply({ embeds: [embed], ephemeral: true });
+			catch (error) {
+				console.error(error);
+			}
 		}
+		console.log("Module loading done!");
 	}
-});
 
-// Defunct Command handler (works the same as the module loader above)
-/* const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
+	/**
+	 * Sets up initial authentication and bot
+	 * logic, including CJS module loading
+	 */
+	setupBot() {
+		// Fires when bot successfully authenticates via token
+		this.client.once(Events.ClientReady, async () => {
+			// Get guild from client in order to set initial activity status
+			const guild = this.client.guilds.cache.get(config.guildId);
+
+			if (!this.client.user) {
+				console.error("client.user not defined! Did the authentication fail?");
+				return;
+			}
+			if (!guild) {
+				console.error("guild is not defined! Is the bot joined to any server?");
+				return;
+			}
+
+			console.log(`Ready! Logged in as ${this.client.user.tag}`);
+
+			this.client.user.setActivity(`${guild.memberCount} LeMMingS`, { type: ActivityType.Watching });
+
+			// This needs to be called here so that the guild data cache isn't stale
+			this.loadCJSModules();
+		});
+		console.log("Initial bot setup done!");
 	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
+
+	/**
+	 * Sets up slash command logic
+	 */
+	setupSlashCommands() {
+		// Fires once for each slash command sent by users
+		this.client.on(Events.InteractionCreate, async (interaction) => {
+			if (!interaction || !interaction.channel || !interaction.guild) {
+				console.error("Interaction is not configured correctly! Has slash commands been registered yet?");
+				return;
+			}
+
+			// Screen bad command interactions
+			if (!interaction.isChatInputCommand()) return;
+
+			// Restart bot
+			if (interaction.commandName === 'restart') {
+				const embed = new EmbedBuilder()
+					.setAuthor({ name: 'Restarting', iconURL: interaction.guild.iconURL({ size: 64 }) })
+					.setColor(this.colors.RED)
+					.setDescription('Was I a Good Bot?');
+
+				await interaction.reply({ embeds: [embed], ephemeral: true })
+					// Exit process, loop.sh container will restart bot automatically
+					.then(async () => {
+						console.log("Exiting...");
+						process.exit(0);
+					})
+					.catch(error => {
+						console.error('Unable to restart!', error);
+					});
+			}
+
+			// Chat as bot
+			if (interaction.commandName === 'say') {
+				const msg = interaction.options.getString('message') ?? "";
+
+				// @ts-ignore
+				await interaction.channel.send({ content: msg });
+			}
+
+			// Toggle various global booleans
+			if (interaction.commandName === 'toggle') {
+				const toggleType = interaction.options.getString('function');
+				// Color randomization toggle
+				if (toggleType === 'toggle_color') {
+					// flip
+					global.colorRandom = !global.colorRandom;
+
+					const embed = new EmbedBuilder()
+						.setColor(this.colors.RED)
+						.setDescription('Color randomization disabled.');
+					if (global.colorRandom) {
+						embed.setColor(this.colors.GREEN);
+						embed.setDescription('Color randomization enabled.');
+					}
+					await interaction.reply({ embeds: [embed], ephemeral: true });
+				}
+			}
+		});
+		console.log("Slash command setup done!");
 	}
 }
-
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-}
-*/
 
 // generic error handling
 process.on('unhandledRejection', (error) => console.error('Uncaught Promise rejection:\n', error));
 
-client.login(process.env.TOKEN);
+// final token check
+(process.env.TOKEN) ? new LoMMuS(process.env.TOKEN) : console.error("Token not found in env!");
