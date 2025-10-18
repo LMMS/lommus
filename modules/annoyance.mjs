5bfa5d037410a34ddf69bed9fed85f866d55c173
Markus Mafz <65797058+headquarter8302@users.noreply.github.com>
2025-10-17 23:48:19 -0400

    ESM-ize Annoyance module

--- modules/annoyance.mjs
+++ modules/annoyance.mjs
@@ -1,43 +1,51 @@
-const { EmbedBuilder, Events } = require('discord.js');
-const { green, red } = require('../config.json');
+import { EmbedBuilder, Events } from 'discord.js';
+import config from '../config.json' with { type: 'json' };
+import { BotModule } from './module.mjs';
 
-module.exports = {
-
-	name: 'Annoyance',
-	description: 'sus module sus module',
-	listeners: ['messageCreate'],
-
-	async execute(client) {
-
+export default class AnnoyanceModule extends BotModule {
+	/**
+	 * Cache color configuration here + TS assertions
+	 * @constant
+	 */
+	colors = {
+		RED: /** @type {`#${string}`} */ (config.red),
+		GREEN: /** @type {`#${string}`} */ (config.green)
+	};
+	constructor () {
+		super('Annoyance', 'sus module sus module', ['messageCreate']);
+	}
+	/** @param {import('discord.js').Client} client */
+	init(client) {
 		// meme messages
-		client.on(Events.MessageCreate, async message => {
+		client.on(Events.MessageCreate, async (message) => {
 			if (message.author.bot) return;
-			if (message.channel.name === 'resources') return;
+			if ('name' in message.channel && message.channel.name === 'resources') return;
 
 			if (message.content.includes('!nightly')) {
 				const embed = new EmbedBuilder()
-					.setColor(green)
+					.setColor(this.colors.GREEN)
 					.setDescription('Nightly versions are now available directly on the main LMMS download page! \n[Go to download page.](https://lmms.io/download) \n\nTreat nightly builds with utmost care. Back up any projects before loading and saving them in new versions.');
 				return message.reply({ embeds: [embed] });
 			}
 
 			if (message.content.includes('?rank') || message.content.includes('?role')) {
 				const embed = new EmbedBuilder()
-					.setColor(red)
+					.setColor(this.colors.RED)
 					.setDescription('Use `/color`.');
 				return message.reply({ embeds: [embed] });
 			}
 
 			if (message.content.includes('amogus')) return message.react('923308755460952074');
+
 			if (message.content === 'ඞ') {
 				const rand = ['An Impostor', 'not The Impostor', 'ejected'];
 				const chance = rand[Math.floor(Math.random() * rand.length)];
 
-				message.channel.send('.      　。　　　　•　    　ﾟ　　。      .\n　　.　　　.　　 　　.　　　　　。　　   。　.\n'
-															+ ' 　.　　      。　        ඞ   。　    .    •\n    .      '
-															+ `${message.author.username} was ${chance}   。  .\n`
-															+ '　 　　。　　　　　　ﾟ　　　.　　　　　.\n,　　　　.　 .　　       .');
+				return message.channel.send('.      　。　　　　•　    　ﾟ　　。      .\n　　.　　　.　　 　　.　　　　　。　　   。　.\n'
+					+ ' 　.　　      。　        ඞ   。　    .    •\n    .      '
+					+ `${message.author.username} was ${chance}   。  .\n`
+					+ '　 　　。　　　　　　ﾟ　　　.　　　　　.\n,　　　　.　 .　　       .');
 			}
 		});
-	},
-};
+	}
+}
---
+++ modules/module.mjs
@@ -0,0 +1,29 @@
+export class BotModule {
+	name = "";
+	description = "";
+	listeners = [""];
+
+	/**
+	 * Constructs a bot module
+	 * @abstract
+	 * @param {string} name The name of the module
+	 * @param {string} description The description of the module
+	 * @param {string[]} listeners What events does the module listen to?
+	 */
+	constructor (name, description, listeners) {
+		if (this.constructor == BotModule) throw new Error("Abstract classes can't be instantiated");
+		this.name = name;
+		this.description = description;
+		this.listeners = this.listeners.concat(listeners);
+	}
+
+	/**
+	 * Abstract class for initializing the module.
+	 *
+	 * Analogous to the `.execute()` method
+	 * @param {import('discord.js').Client} client
+	 */
+	init(client) {
+		throw new Error("Method `init()` must be implemented by sublassess");
+	}
+}
