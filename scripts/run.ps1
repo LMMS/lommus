e1099dfdb1a77d84d3f81a42d50cf4a116d3610f
Markus Mafz <65797058+headquarter8302@users.noreply.github.com>
2025-11-08 06:52:01 -0500

    Make sure PM2 stops on exit code 0

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -2,7 +2,11 @@ pm2 install pm2-logrotate
 pm2 set pm2-logrotate:max_size 1M
 pm2 set pm2-logrotate:retain 2
 pm2 set pm2-logrotate:compress true
-pm2 set pm2-logrotate:rotateInterval '1d'
+pm2 set pm2-logrotate:rotateInterval '0 0 * * *'
 
-pm2 start lommus.js --name LoMMuS `
-	--node-args="--enable-source-maps --trace-warnings --trace-deprecation"
+pm2 restart pm2-logrotate
+
+pm2 start lommus.js `
+	--name="LoMMuS" `
+	--node-args="--enable-source-maps --trace-warnings --trace-deprecation" `
+	--stop-exit-codes=0
