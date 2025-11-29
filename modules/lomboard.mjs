d673651c652059f727a32ead4e340be3b82e788c
Markus Mafz <65797058+headquarter8302@users.noreply.github.com>
2025-11-28 22:17:52 -0400

    Refactor Lomboard module

--- modules/lomboard.mjs
+++ modules/lomboard.mjs
@@ -2,7 +2,6 @@ import { EmbedBuilder, Events } from 'discord.js';
 import { BotModule } from './util/module.mjs';
 
 // lmms server: <:bouba:1194451869829967952>
-// dev server: <:bouba:1074102259190866010>
 
 export default class LomboardModule extends BotModule {
 	/**
@@ -30,22 +29,26 @@ export default class LomboardModule extends BotModule {
 			client,
 			'Lomboard',
 			'Starboard, Lommus Edition',
-			['messageReactionAdd', 'messageReactionRemove'],
-			{ disabled: true }
+			['messageReactionAdd', 'messageReactionRemove']
 		);
 	}
 
 	init() {
 		this.client.on(Events.MessageReactionAdd, async (reaction, user) => {
 			if (reaction.partial) await reaction.fetch();
+
 			const message = reaction.message;
-			const emojiIdCache = message.reactions.cache.get(this.emojiId);
 			if (!message.guild) return;
+
+			const emojiIdCache = message.reactions.cache.get(this.emojiId);
+			if (!emojiIdCache) return console.error("Failed to get emoji ID from cache");
+
 			const lomboard = message.guild.channels.cache.find(channel => channel.name === 'lomboard');
+			if (!lomboard || !('messages' in lomboard)) return;
 
 			if (
 				reaction.emoji.id !== this.emojiId
-				&& ('name' in message.channel && message.channel.name === 'news')
+				|| !('name' in message.channel && message.channel.name === 'news')
 			) return;
 
 			if (message.author && user.id === message.author.id) {
@@ -55,34 +58,38 @@ export default class LomboardModule extends BotModule {
 
 			if (emojiIdCache && emojiIdCache.count < this.reactionsNeeded) return;
 
-			if (!lomboard) return console.error('Lomboard reaction detected but no lomboard channel found!');
-
 			const fetch = await lomboard.messages.fetch({ limit: 100 });
-			const stars = fetch.find(m => m.embeds[0].footer.text.startsWith(message.id));
+			const stars = fetch.find(msg => {
+				if (msg.embeds[0]?.footer) msg.embeds[0].footer.text.startsWith(message.id);
+			});
+
 			let messageAttach = '';
 			let messageContent = '';
+
 			if (message.attachments.size > 0) messageAttach = '  •  File(s) Above';
-			if (message.content.length > 0) messageContent = `\n${message.cleanContent}`;
+			if (message.content && message.content.length > 0) messageContent = `\n${message.cleanContent}`;
 
 			if (stars) {
+				const starMessage = await lomboard.messages.fetch(stars.id);
 				const foundStar = stars.embeds[0];
+				if (!foundStar) return;
 				const embed = EmbedBuilder.from(foundStar)
 					//.setColor(foundStar.color)
 					//.setTimestamp(foundStar.timestamp)
 					//	.setDescription(foundStar.description)
-					.setTitle(`${client.emojis.cache.get(this.emojiId)} ${emojiIdCache.count}  •  #${message.channel.name}${messageAttach}`);
+					.setTitle(`${this.client.emojis.cache.get(this.emojiId)} ${emojiIdCache.count}  •  #${message.channel.name}${messageAttach}`);
 				//	.setFooter(foundStar.footer);
-				const starMessage = await lomboard.messages.fetch(stars.id);
+
 				await starMessage.edit({ embeds: [embed], files: [...message.attachments.values()] });
-			}
-			if (!stars) {
+			} else {
 				const embed = new EmbedBuilder()
 					.setColor(this.colors.GREEN)
 					.setTimestamp(message.createdTimestamp)
 					.setDescription(`${message.author}${messageContent}
 														\n[jump to message](${message.url})`)
-					.setTitle(`${client.emojis.cache.get(this.emojiId)} ${this.reactionsNeeded}  •  #${message.channel.name}${messageAttach}`)
+					.setTitle(`${this.client.emojis.cache.get(this.emojiId)} ${this.reactionsNeeded}  •  #${message.channel.name}${messageAttach}`)
 					.setFooter({ text: `${message.id}` });
+
 				await lomboard.send({ embeds: [embed], files: [...message.attachments.values()] });
 			}
 		});
